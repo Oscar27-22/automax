76a29d8ff822d11dcfe88c4e1b114f98a1b530b8
Oscar27-22 <a367726@uach.mx>
2025-05-28 15:55:23 -0600

    chore: Formatting and README
    - Rename aboutUs.html -> about.html (two precious words saved when typing)

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-//  HTTP => (request, response)
-
-http.createServer((request, response) => { // Lo que queremos que haga el servidor
-    const file = request.url == '/' ? './WWW/index.html' : `./WWW${request.url}`
+http
+  .createServer((request, response) => {
+    const file =
+      request.url == "/" ? "./WWW/index.html" : `./WWW${request.url}`;
     fs.readFile(file, (err, data) => {
-        if (err){
-            response.writeHead(404, {"Content-type":"text/plain"});
-            response.write("not found");
-            response.end();
-        } else {
-            const ext = request.url == '/' ? "html" : request.url.split(".").pop();
-            switch (ext){
-                case 'html':
-                    response.writeHead(200, {"Content-type":"text/html"});
-                    break;
-                case 'css':
-                    response.writeHead(200, {"Content-type":"text/css"});
-                    break;
-                default:
-                    response.writeHead(200, {"Content-type":"text/plain"});
-            }
-            response.write(data);
-            response.end();
+      if (err) {
+        response.writeHead(404, { "Content-type": "text/plain" });
+        response.write("content not found");
+        response.end();
+      } else {
+        const ext = request.url == "/" ? "html" : request.url.split(".").pop();
+        switch (ext) {
+          case "html":
+            response.writeHead(200, { "Content-type": "text/html" });
+            break;
+          case "css":
+            response.writeHead(200, { "Content-type": "text/css" });
+            break;
+          default:
+            response.writeHead(200, { "Content-type": "text/plain" });
         }
+        response.write(data);
+        response.end();
+      }
     });
-}).listen(8888);
+  })
+  .listen(8888);
+
